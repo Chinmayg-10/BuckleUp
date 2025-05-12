3e7fba70d2734fb5b1d4f888eae50b0adfda3bed
Chinmayg-10 <chinmaygupta032@gmail.com>
2025-05-12 16:40:38 +0530

    initial commit

---
+++ Balanz.io/backend/index.js
@@ -0,0 +1,46 @@
+const mongoose=require('mongoose')
+const express = require("express");
+const dotenv = require("dotenv");
+const cors=require("cors");
+dotenv.config();
+const routes=require("./routes/todo");
+const userRoutes=require("./routes/user");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+// app.use(cors({
+//     donains: ["https://balanz-io.vercel.app/login","https://balanz-io.vercel.app/signup","https://balanz-io.vercel.app","https://balanz-io.vercel.app/todos"];
+//         ));
+app.use(cors());
+app.use(express.static(path.join(__dirname,'../public')));
+
+app.use(userRoutes);
+app.use(routes);//this middleware allows acess to  the modular routes i've created
+
+app.get("/healthy", (req, res)=> res.send("I am Healthy"));
+
+//  start writing your routes here
+app.get("*",(req,res)=>{
+    res.status(404).sendFile(path.join(__dirname,"../public/notFound.html"))
+})
+
+// app.get('/home',  (req, res) => {
+//     // Implement logic for getting todos for a user
+//     res.sendFile(path.join(__dirname,"../../public/landing.html"));
+// });
+
+
+app.listen(port, ()=> {
+    mongoose.connect(process.env.MONGO_DB)
+    .then(()=>{
+        console.log(`server is running at http://localhost:${port}`)
+    })
+    .catch((err)=>{
+        console.log(`error in connecting to mongo db \n ${err}`)
+    })
+});
+
